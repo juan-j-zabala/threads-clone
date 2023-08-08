d280ceca46a4b61c22f4ca3b10b57b8d2aae1e85
juan jose zabala <juanzabala317@hotmail.com>
2023-08-08 14:28:22 -0500

    second commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,12 @@
-
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <>
-      <h1 className="head-text text-left  ">Home</h1>
+      <h1 className="head-text text-left  ">
+        Home
+        {/* <UserButton afterSignOutUrl="/"/> */}
+      </h1>
     </>
   )
 }
