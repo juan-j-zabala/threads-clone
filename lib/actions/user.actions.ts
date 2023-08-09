a02c18e401ede03fed33845939633e664ab4c2a5
juan jose zabala <juanzabala317@hotmail.com>
2023-08-09 15:59:42 -0500

    quinto commit

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,9 @@
+"user server"
+
+import User from "../models/user.model"
+import { connectToDB } from "../mongoose"
+
+export async function updateUser():Promise<void> {
+    connectToDB
+    await User.findOneAndUpdate({})
+}
